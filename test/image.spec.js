534a9c31a6e74dd72a81f8e9820d1a571d62efcc
Nir Maoz <maoznir@gmail.com>
2020-04-04 21:41:26 +0300

    Add minified build

--- test/image.spec.js
+++ test/image.spec.js
@@ -3,11 +3,18 @@ import { render } from '@testing-library/svelte'
 import Image from '../src/Image.svelte'
 
 describe('Image', () => {
-  test('src attribute should include cloud_name & public_id', () => {
+  it('src attribute should include cloud_name & public_id', () => {
     const { container } = render(Image, { cloud_name: 'demo', public_id: 'sample' });
     const img = container.querySelector("img");
     expect(img.src).toBe(
       'http://res.cloudinary.com/demo/image/upload/sample'
+    );
+  });
+  it('alt attribute should be set to given alt value', () => {
+    const { container } = render(Image, { cloud_name: 'demo', public_id: 'sample', alt: 'test image' });
+    const img = container.querySelector("img");
+    expect(img.alt).toBe(
+      'test image'
     );
   });
 });
